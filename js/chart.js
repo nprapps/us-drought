2f5da30658dd4a7fd178f1580e3536bcf8f988f0
Danny DeBelius <danny.debelius@gmail.com>
2012-07-18 17:50:54 -0400

    Fix IE

--- js/chart.js
+++ js/chart.js
@@ -2,11 +2,11 @@ String.prototype.commafy = function () {
   return this.replace(/(^|[^\w.])(\d{4,})/g, function($0, $1, $2) {
     return $1 + $2.replace(/\d(?=(?:\d\d\d)+(?!\d))/g, "$&,");
   });
-}
+};
 
 Number.prototype.commafy = function () {
   return String(this).commafy();
-}
+};
 
 var chart = new Highcharts.Chart({
     chart: {
@@ -77,9 +77,6 @@ var chart = new Highcharts.Chart({
             });
 
             return s;
-        },
-        style: {
-          font: '"Helvetica Neue",Helvetica,Arial,sans-serif'
         }
     },
     xAxis: {
